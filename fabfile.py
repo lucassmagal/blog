a3765e1862ff35e9fe36fe75611b73f319c7781f
Lucas Sampaio <magal.lucas@gmail.com>
2012-11-19 01:57:15 -0200

    script para fazer deploy está pronto

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,7 @@
 import os
 from fabric.api import local, lcd
 import glob
-from datetime import datetime
+from datetime import date
 
 
 def build():
@@ -23,9 +23,9 @@ def build():
         local('cp %s output' % path)
 
     with lcd('output'):
-        commit_message = "DEPLOY %s" % datetime.now()
+        commit_message = "DEPLOY %s" % date.today().strftime("%Y_%m_%d")
 
         local('git add .')
-        local('git commit -m %s' % commit_message)
+        local('git commit -m "%s"' % commit_message)
 
         local('git push heroku master')
