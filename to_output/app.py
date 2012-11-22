638bf4ada1a3fd992c5219d2c9df45383f9a934b
Lucas Sampaio <magal.lucas@gmail.com>
2012-11-21 22:12:21 -0200

    Teste simples de simplificação do app.py

--- to_output/app.py
+++ to_output/app.py
@@ -4,19 +4,20 @@ import os
 from flask import Flask, send_file
 
 DEBUG = False
+
 app = Flask(__name__, static_folder='build/static')
 app.config.from_object(__name__)
 
 
-@app.route('/')
-def index():
-    return send_file('build/index.html')
+# @app.route('/')
+# def index():
+#     return send_file('build/index.html', cache_timeout=60)
 
 
 @app.route('/<path:path>/')
 def application(path):
     path = "build/%s/index.html" % path
-    return send_file(path)
+    return send_file(path, cache_timeout=60)
 
 
 if __name__ == '__main__':
